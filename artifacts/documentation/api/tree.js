e5942e79be9435bb85a4cc6f2047d6d819bcfc62
Woodhouse <contact@icecave.com.au>
2014-06-13 00:12:42 +0000

    Publishing artifacts from build 3.

--- artifacts/documentation/api/tree.js
+++ artifacts/documentation/api/tree.js
@@ -1,6 +1,9 @@
 var tree = [["Icecave",,"",
                 [["Chrono",,"",
                 [["Doctrine","Icecave\/Chrono\/Doctrine.html","",
-                [["PackageInfo","Icecave\/Chrono\/Doctrine\/PackageInfo.html","",
+                [["DateTimeType","Icecave\/Chrono\/Doctrine\/DateTimeType.html"," < DateTimeType",
+                [        ]],["DateType","Icecave\/Chrono\/Doctrine\/DateType.html"," < DateType",
+                [        ]],["DoctrineTypeInstaller","Icecave\/Chrono\/Doctrine\/DoctrineTypeInstaller.html","",
+                [        ]],["PackageInfo","Icecave\/Chrono\/Doctrine\/PackageInfo.html","",
                 [        ]]        ]]        ]]        ]]        ]
 
